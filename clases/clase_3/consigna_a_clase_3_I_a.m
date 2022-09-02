ae348ce18c5e704e5a0ec5dc87dc25876e953e6c
alanshalem <alanshalem@gmail.com>
2022-09-02 12:55:27 -0300

    fix: vuelvo para atras las funciones que estaban mal

--- clases/clase_3/consigna_a_clase_3_I_a.m
+++ clases/clase_3/consigna_a_clase_3_I_a.m
@@ -16,9 +16,9 @@ t = -6:dt:12;
 x_t = rampa(t - 1) - rampa(t - 2) - 2 * escalon(t - 3) + escalon(t - 4);
 x_t_a_plus = rampa((t + 2) - 1) - rampa((t + 2) - 2) + 2 * escalon((t + 2) - 3) + escalon((t + 2) - 4);
 x_t_a_minus = rampa((t - 2) - 1) - rampa((t - 2) - 4) + 2 * escalon((t - 2) - 3) + escalon((t - 2) - 4);
-x_t_b = rampa(3 * (t - 1)) - rampa(3 * (t - 2)) + 2 * escalon(3 * (t - 3)) + escalon(3 * (t - 4));
-x_t_c = rampa(-2 * (t - 1) + 1) - rampa(-2 * (t - 2) + 1) + 2 * escalon(-2 * (t - 3) + 1) + escalon(-2 * (t - 4) + 1);
-x_t_d = rampa(((t - 1) / 2) -2) - rampa(((t - 2) / 2) - 2) + 2 * escalon((t - 3) / 2 - 2) + escalon(((t - 4) / 2) - 2);
+x_t_b = rampa((3 * t) - 1) - rampa((3 * t) - 2) + 2 * escalon((3 * t) - 3) + escalon((3 * t) - 4);
+x_t_c = rampa((-2 * t + 1) - 1) - rampa((-2 * t + 1) - 2) + 2 * escalon((-2 * t + 1) - 3) + escalon((-2 * t + 1) - 4);
+x_t_d = rampa((t / 2) - 1) - rampa((t / 2) - 2) + 2 * escalon((t / 2) - 3) + escalon((t / 2) - 4);
 
 %% Graficos
 subplot(4, 2, [1 2]);
