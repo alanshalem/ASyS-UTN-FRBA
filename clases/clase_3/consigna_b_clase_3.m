711650cc6965f54172dcea01079e33de148f3bfc
alanshalem <alanshalem@gmail.com>
2022-09-02 11:59:49 -0300

    fix: corregido consigna b, falta arreglar potencia 4

--- clases/clase_3/consigna_b_clase_3.m
+++ clases/clase_3/consigna_b_clase_3.m
@@ -1,6 +1,6 @@
 % # Consigna de la clase #B (20 minutos) pag45
 %
-% 1. Graficar las siguientes señales en MatLab y calcular numéricamente su potencia o energía según sea el caso:  
+% 1. Graficar las siguientes señales en MatLab y calcular numéricamente su potencia o energía según sea el caso:
 %    a) x1(t)=e^(-|t|)
 %
 %    b) x2(t)=sen(2*pi*5t)+cos(2*pi*10t)
@@ -9,44 +9,95 @@
 %
 %    d) x2[n]=cos(pi/2*n)
 %
-% 2. Verificar analíticamente la totalidad de los resultados obtenidos.
+% 2. Verificar analiticamente la totalidad de los resultados obtenidos.
 
 % ###############################################################################################
 
 %% Resolucion
+
+%% a)
 dt = 0.01;
-t=-20:dt:20;
-x1=exp(-abs(t));
-Ex1 = ENERGIA(x1, dt);
+t = -20:dt:20;
+x1 = exp(-abs(t));
+EA_1 = ENERGIA(x1, dt); % Ex1 = 1.0000
+% EA_2 = sum(abs(x1).^2)*dt
+% EA_3 = trapz(t, abs(x1).^2)
 
-x2=sin(2*pi*5*t)+cos(2*pi*10*t);
-x3=((1/2).^(t).*escalon(t));
-x4=cos((pi/2)*t);
+% Otra forma de calcular la energia (discretizando el vector temporal)
+% n=-10:10
+% x1=exp(-abs(n))
+% EA_4 = sum(abs(x1).^2)*1
 
-%% Graficos
-subplot(2,2,1)
-plot(t,x1)
-title('x1(t)')
-xlabel('t')
-ylabel('x1(t)')
+% Grafico a)
+grid on;
+axis tight;
+subplot(2, 2, 1);
+plot(t, x1, 'r', 'linewidth', 2);
+title('x1(t)=exp(-abs(t))');
+xlabel('t');
+ylabel('x1(t)');
 
-subplot(2,2,2)
-plot(t,x2)
-title('x2(t)')
-xlabel('t')
-ylabel('x2(t)')
-xlim([0 2])
-ylim([-2.5 1.5])
+%% b)
+% x2=sin(2*pi*5*t)+cos(2*pi*10*t);
+% Como grafico bien esta funcion utilizando los conceptos?
+f01 = 5; % frecuencia 1
+f02 = 10; % frecuencia 2
+w01 = 2 * pi * f01; % velocidad angular 1
+w02 = 2 * pi * f02; % velocidad angular 2
+f0 = gcd(f01, f02); % MCD de las frecuencias es 5 - frecuencia de la señal resultante
+T0 = 1/5; % Periodo de la señal resultante
+fs = 30 * f0; % Frecuencia de muestreo
+F0 = f0 / fs; % Frecuencia Digital
+dt_x2 = 1 / fs; % Defino el paso temporal como la inversa de mi frecuencia de muestreo
+t_x2 = (-2 * T0 - dt_x2):dt_x2:(3 * T0 - dt_x2); % Defino el vector intervalo temporal
+x2 = sin(w01 * t_x2) + cos(w02 * t_x2);
 
-subplot(2,2,3)
-plot(t,x3)
-title('x3(t)')
-xlabel('t')
-ylabel('x3(t)')
+% Grafico b)
+grid on;
+axis tight;
+subplot(2, 2, 2);
+plot(t_x2, x2, 'r', 'linewidth', 2);
+title('x2=sin(2*pi*5*t)+cos(2*pi*10*t)');
+xlabel('t');
+ylabel('x2(t)');
+% Para utilizar la funcion potencia es necesario pasarle un solo periodo
+% Esta es la forma de aislar un periodo
+tt = 0:dt_x2:T0 - dt;
+% Otra forma mas engorrosa.
+x2_n_zero = -t_x2(1) / dt_x2 + 1;
+x2_n_end = x2_n_zero + T0 / dt_x2 - 1;
+x2_ciclo_slice = x2_n_zero:x2_n_end;
 
-subplot(2,2,4)
-plot(t,x4)
-title('x4(t)')
-xlabel('t')
-ylabel('x4(t)')
+POT2 = POTENCIA(x2, T0, dt_x2);
 
+%% c) digital, senal de energia
+n = -5:20;
+x3 = ((1/2).^(n) .* escalon(n));
+EC = ENERGIA(x3, 1);
+
+% Grafico c)
+grid on;
+axis tight;
+subplot(2, 2, 3)
+stem(n, x3, 'linewidth', 2);
+title('x3=((1/2)*exp((n)).*escalon(n));');
+xlabel('n');
+ylabel('x3[n]');
+
+%% d)
+dn = 1;
+n = -15:dn:16;
+fo = pi / 2;
+To = 1 / fo;
+x4 = cos((fo) * n);
+POT4 = POTENCIA(x4, To, dn);
+
+% Grafico d)
+grid on;
+axis tight;
+subplot(2, 2, 4);
+stem(n, x4, 'linewidth', 2);
+title('x2[n]=cos(pi/2*n)');
+xlabel('t');
+ylabel('x4(t)');
+xlim([-15 16]);
